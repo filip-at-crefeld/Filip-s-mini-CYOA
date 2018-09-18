89cad51a6461e7ba6cae3af75a0c630367be0cff
Filip Furmanek <ffurmanek@crefeld.org>
2018-09-18 12:44:34 -0400

    End of class 180918, commenting and page texts
    We started adding comments line by line to explain what the python code did.
    
    We also changed the page texts to be self-descriptive, and to mention the choices.

--- game_main.py
+++ game_main.py
@@ -3,22 +3,46 @@
 
 def print_out_current_page(page_number):
     # TODO: Print to the console the text from that page. Include the choices like 1) xxx 2) xxx.
+
+    # Are we on page 0?
     if page_number == 0:
-        print("yay")
+
+        # Print out stuff on page 0
+        print("This is the contents of page 0.", "0) Go to page 1.")
+
+    # Are we on page 1?
     elif page_number == 1:
-        print("nah")
+
+        # Print out stuff on page 1
+        print("This would be text on page 1.", "0) Go to page 0.")
 
 
 def get_integer_from_player():
     # TODO: Use input function to get a string, make sure it's a number, then return an int.
     # TODO: If it's not a number, do it again.
+
+    # An infinite loop
     while True:
+
+        # Asks for an input and then assigns the string of that to raw_input
         raw_input = input("Choose> ")
+
+        # Marks the start of an error-catching section
         try:
+
+            # Turn the string into an integer // THIS STATEMENT IS RISKY
             num = int(raw_input)
+
+            # Exit out of the function here. ESCAPE!
             return num
+
+        # Ends the error catching section, starts the error handling section
         except ValueError:
+
+            # "Handle" the error by reporting to the player
             print("Please enter a number instead.")
+
+        # This is the end of the while loop, so if you get here start the next loop
 
 
 def find_next_page_from_choice(from_page, choice_number):
